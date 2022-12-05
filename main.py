23f26a0d4e40e6a5ca4a8a81dbb53b5ebd8ffea0
maysaalh <maysaalhomaizi5@gmail.com>
2022-12-05 20:06:21 +0300

    not sure but ok im done

---
+++ main.py
@@ -0,0 +1,18 @@
+def my_name():
+    print("My name is Maysa")
+my_name()
+
+def my_meal(food):
+    food = input("whats your favourite food?")
+    drink = input("what do you like to drink")
+print(f"I like to eat {food} and while drinking {drink}")
+
+
+def cube(number):
+    return number^3
+
+def by_three(number):
+    if number/3:
+    def cube(number)   
+    else:
+     print ("false")
